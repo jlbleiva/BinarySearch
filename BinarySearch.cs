b085adc676aee505e8e03b683ecf6187495825e3
JOSE LUIS BENITEZ <joseluis.benitez@dxc.com>
2021-10-27 15:59:36 +0200

    Agregar archivos de proyecto.

---
+++ BinarySearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value)
+    {
+        int firstpos = 0;
+        int lastpos = input.Length - 1;
+        bool found = false;
+        int middle;
+
+       
+        while (firstpos <= lastpos && found == false)
+        {
+
+            int middleindex = (firstpos + lastpos) / 2;
+            int middlevalue = input[middleindex];
+            if (middlevalue == value) return middleindex;
+
+            if (value < middlevalue)
+            {
+                lastpos = middleindex-1;
+                
+            }
+            else
+            {
+                firstpos =middleindex+1;
+            }
+        }
+       
+        
+
+        return -1;
+
+    }
+}
